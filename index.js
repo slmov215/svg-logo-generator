5031f8214698f9850512ba4c7131f3bae411583b
Stephen <127278005+slmov215@users.noreply.github.com>
2023-05-20 00:55:54 -0400

    started creating files and startup codes

---
+++ index.js
@@ -0,0 +1,12 @@
+// Package needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const question = [];
+
+
+function writeToFile(fileName, data) {};
+
+function init() {};
+
+init();
